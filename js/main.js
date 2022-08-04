96b9f6d6b0b9c143a901edb4b84398eaa242fabb
EgorVertaev <eholife123@gmail.com>
2022-08-04 11:40:28 +0300

    test eslint

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 const func = () => {
-  console.log("some func")
+  console.log('some func');
+};
+
+func();
+
+function foo(name) {
+  const lastName = name;
+  return lastName;
 }
 
-const func = 10
+foo('den');
+
+let x = 200;
+console.log(window);
+console.log(x);
+
+x++;
